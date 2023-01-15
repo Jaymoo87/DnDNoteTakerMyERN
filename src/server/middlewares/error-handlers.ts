c7d08a97816e18c353795c96c01e8df7c457f0a2
Justin Murrah <jdmurrah@gmail.com>
2023-01-15 11:10:53 -0600

    handler update

--- src/server/middlewares/error-handlers.ts
+++ src/server/middlewares/error-handlers.ts
@@ -1,13 +1,11 @@
 import express from "express";
 
-// catch 404s
 export const notFoundHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const error = new Error(`${req.method} ${req.originalUrl} not found`);
   error["status"] = 404;
   next(error);
 };
 
-// 'next' for global error handler
 export const globalErrorHandler = (
   error: Error,
   req: express.Request,
