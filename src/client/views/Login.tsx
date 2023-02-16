b7ea9d7cfaab008ae87d0bdee163990b89041bcc
Justin Murrah <jdmurrah@gmail.com>
2023-02-15 23:13:50 -0600

    some input handling, button to submit and recieve a jwt

--- src/client/views/Login.tsx
+++ src/client/views/Login.tsx
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface LoginProps {}
 
 const Login = (props: LoginProps) => {
+  const [values, setValues] = useState<{ [key: string]: string }>({});
+
+  const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((res) => res.json())
+
+      .then((res) => {
+        localStorage.setItem("token", res.token);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <h1>Login</h1>
       <div>
         <form>
-          <input type="email">
-            <input type="password">
-              <button>Login</button>
-            </input>
-          </input>
+          <input
+            type="email"
+            name="email"
+            value={values.email || ""}
+            onChange={handleChanges}
+            autoComplete="current-email"
+          />
+          <input
+            type="password"
+            name="password"
+            value={values.password || ""}
+            onChange={handleChanges}
+            autoComplete="current-password"
+          />
+          <button onClick={handleClick}>Login</button>
         </form>
       </div>
     </div>
