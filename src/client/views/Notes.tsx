d33d5475fb58ae18afb1db572f35f0a2e3332edf
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-03-14 15:59:32 -0500

    got some background images working, fonts and layout work is next

--- src/client/views/Notes.tsx
+++ src/client/views/Notes.tsx
@@ -16,8 +16,8 @@ const Notes = () => {
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <article className="grid p-4 m-3 shadow-lg rounded-2xl shadow-gray-900 justify-self-center justify-items-center bg-ghost">
+    <div className="container mx-auto ">
+      <article className="bg-[url(https://r4.wallpaperflare.com/wallpaper/403/627/591/action-adventure-dragons-dungeons-wallpaper-267e8af12d15328e8e24c724d24214d1.jpg)] bg-cover grid p-4 m-3 shadow-lg rounded-2xl shadow-gray-900 justify-self-center justify-items-center bg-ghost">
         <h1 className="flex justify-center h-10 p-2 font-serif font-bold text-black rounded shadow w-44 bg-accent">
           Notes
         </h1>
@@ -25,7 +25,10 @@ const Notes = () => {
         <span>click scroll to see the whole damn note</span>
         <div className="flex flex-col-reverse justify-center ">
           {notes.map((note) => (
-            <div className="h-32 p-4 m-2 shadow bg-neutral shadow-slate-800 rounded-xl" key={`note-key-${note.id}`}>
+            <div
+              className="h-32 p-4 m-2 border shadow border-accent bg-neutral shadow-slate-800 rounded-xl"
+              key={`note-key-${note.id}`}
+            >
               <h2 className="pb-3">{note.first_name}'s Note</h2>
 
               <p className="flex justify-center px-5">{note.body.slice(0, 125)} ...</p>
