e341318847ac839e9ab21aadc2b5fe968720fda4
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-03-15 13:46:31 -0500

    got banner working...page background droped though

--- src/client/views/Notes.tsx
+++ src/client/views/Notes.tsx
@@ -18,7 +18,7 @@ const Notes = () => {
   return (
     <div className="container mx-auto ">
       <article className="bg-[url(https://r4.wallpaperflare.com/wallpaper/403/627/591/action-adventure-dragons-dungeons-wallpaper-267e8af12d15328e8e24c724d24214d1.jpg)] bg-cover grid p-4 m-3 shadow-lg rounded-2xl shadow-gray-900 justify-self-center justify-items-center bg-ghost">
-        <h1 className="flex justify-center h-10 p-2 font-serif font-bold text-black rounded shadow w-44 bg-accent">
+        <h1 className="flex justify-center h-10 p-2 font-serif font-bold text-black rounded shadow w-44 bg-[url(../pictures/banner.png)] bg-center bg-contain bg-no-repeat">
           Notes
         </h1>
 
@@ -32,10 +32,11 @@ const Notes = () => {
               <h2 className="pb-3">{note.first_name}'s Note</h2>
 
               <p className="flex justify-center px-5">{note.body.slice(0, 125)} ...</p>
-              {/* <button className=" btn btn-primary btn-sm">View This Note</button> */}
-              <Link to={`/notes/${note.id}`} className="flex justify-end">
-                <GiTiedScroll className="mt-2 text-2xl" />
-              </Link>
+              <div className="flex justify-end">
+                <Link to={`/notes/${note.id}`}>
+                  <GiTiedScroll className="m-2 mt-2 text-2xl" />
+                </Link>
+              </div>
             </div>
           ))}
         </div>
