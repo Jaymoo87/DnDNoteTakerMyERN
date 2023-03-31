71a8dc60f94375ec6ed860521f3cf7fedb41ee3f
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-03-31 10:46:15 -0500

    deps

--- src/client/components/Container.tsx
+++ src/client/components/Container.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Container = () => {
-  return <div className="flex flex-col px-2 mx-auto md:px-1 sm:px-0"></div>;
+  return (
+    <>
+      <div className="flex flex-col px-2 mx-auto md:px-1 sm:px-0"></div>
+    </>
+  );
 };
 
 export default Container;
