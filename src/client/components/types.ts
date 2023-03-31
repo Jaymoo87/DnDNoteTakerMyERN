7891c69f90abad78c490d637ef1a4d88b6b28513
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-03-31 10:31:52 -0500

    fixed typings for components

--- src/client/components/types.ts
+++ src/client/components/types.ts
@@ -1,2 +1,4 @@
-export const ComponentColors = [];
-export const ComponentSizes = [];
+import { componentColors, componentSizes } from "./constants";
+
+export type ComponentColors = typeof componentColors[number];
+export type ComponentSizes = typeof componentSizes[number];
