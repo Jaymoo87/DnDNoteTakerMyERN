b78d581fc8915bc07fe092ce97b0e954110e7689
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-03-31 10:19:45 -0500

    part 16, some sweet Typescript utilizing and some styling-assist libraries

--- src/client/views/Login.tsx
+++ src/client/views/Login.tsx
@@ -5,6 +5,7 @@ import authService from "../services/auth";
 import { useForm } from "../utilities/use-form";
 import { useAuth } from "../utilities/use-auth";
 import Input from "../components/Input";
+import { Container } from "../components";
 
 interface LoginProps {}
 
@@ -25,10 +26,14 @@ const Login = (props: LoginProps) => {
   };
 
   return (
-    <div>
+    <Container>
       <h1>Login</h1>
-      <div>
-        <form>
+
+      <form className="flex flex-col items-center justify-center">
+        <div className="w-full max-w-xs form-control">
+          <label htmlFor="" className="label">
+            <span className="label-text">Email</span>
+          </label>
           <Input
             type="email"
             name="email"
@@ -36,19 +41,27 @@ const Login = (props: LoginProps) => {
             onChange={handleChanges}
             autoComplete="current-email"
           />
+        </div>
+        <div className="w-full max-w-xs form-control">
+          <label htmlFor="" className="label">
+            <span className="label-text">Password</span>
+          </label>
           <Input
             type="password"
             name="password"
             value={values.password || ""}
             onChange={handleChanges}
             autoComplete="current-password"
+            bordered={false}
           />
-          <button onClick={handleClick}>Login</button>
-        </form>
-        {location.state?.message && <div>{location.state?.message}</div>}
-        {error && <div>{error}</div>}
-      </div>
-    </div>
+        </div>
+        <button onClick={handleClick} className="mt-3 btn btn-primary btn-wide">
+          Login
+        </button>
+      </form>
+      {location.state?.message && <div>{location.state?.message}</div>}
+      {error && <div>{error}</div>}
+    </Container>
   );
 };
 
